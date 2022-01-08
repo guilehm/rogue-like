14d0b9e475dfc56b832650f81b927aa9774b24a3
guilherme <guile.hm@hotmail.com>
2022-01-07 21:31:43 -0300

    listen to register and unregister channels

--- src/models/ws-models.go
+++ src/models/ws-models.go
@@ -13,9 +13,22 @@ type Client struct {
 }
 
 type Hub struct {
-	Clients map[*Client]bool
+	Clients    map[*Client]bool
+	Register   chan *Client
+	Unregister chan *Client
 }
 
 func (hub *Hub) Start() {
 	fmt.Println("starting")
+	for {
+		select {
+		case client := <-hub.Register:
+			log.Println("registering client")
+			hub.Clients[client] = true
+		case client := <-hub.Unregister:
+			if _, ok := hub.Clients[client]; ok {
+				delete(hub.Clients, client)
+			}
+		}
+	}
 }
