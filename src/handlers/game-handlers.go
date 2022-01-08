0f16656a112ea3de005d4c9c50ea32eb4acc5256
guilherme <guile.hm@hotmail.com>
2022-01-07 21:38:29 -0300

    start listening to ws messages

--- src/handlers/game-handlers.go
+++ src/handlers/game-handlers.go
@@ -1,6 +1,8 @@
 package handlers
 
 import (
+	"errors"
+	"fmt"
 	"log"
 	"net/http"
 	"rogue-like/models"
@@ -14,10 +16,26 @@ func RogueLikeHandler(hub *models.Hub, w http.ResponseWriter, r *http.Request) {
 	// TODO: do not allow all origins
 	upgrader.CheckOrigin = func(r *http.Request) bool { return true }
 
+	quit := make(chan bool)
 	conn, err := upgrader.Upgrade(w, r, nil)
 	defer conn.Close()
 	if err != nil {
 		log.Println("Error during connection upgrade:", err)
 		return
+	}
+
+	for {
+		message := models.WSMessage{}
+		err = conn.ReadJSON(&message)
+		if err != nil {
+			if errors.Is(err.(*websocket.CloseError), err) {
+				log.Println("Connection closed")
+				quit <- true
+				return
+			} else {
+				fmt.Println("Could not read message:", err)
+				continue
+			}
+		}
 	}
 }
