081612fd8efa8f7509f981b5fc77402ec4f3b420
guilherme <guile.hm@hotmail.com>
2022-01-08 00:48:21 -0300

    create channels

--- src/main.go
+++ src/main.go
@@ -17,12 +17,16 @@ func main() {
 	r := mux.NewRouter()
 	service := services.GameService{
 		Hub: &models.Hub{
-			Clients: make(map[*models.Client]bool),
+			Clients:    make(map[*models.Client]bool),
+			Register:   make(chan *models.Client),
+			Unregister: make(chan *models.Client),
+			Broadcast:  make(chan bool),
+			Sprites:    []models.Sprite{},
 		},
 	}
 	go service.Start()
 
-	r.HandleFunc("/ws/rogue-like/", func(w http.ResponseWriter, r *http.Request) {
+	r.HandleFunc("/ws/rogue/", func(w http.ResponseWriter, r *http.Request) {
 		handlers.RogueLikeHandler(&service, w, r)
 	})
 	_ = http.ListenAndServe(":"+os.Getenv("PORT"), r)
