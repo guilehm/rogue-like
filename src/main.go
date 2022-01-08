4b6da289e7eeb138fd74611f4d3d6376333cc0c8
guilherme <guile.hm@hotmail.com>
2022-01-07 21:26:37 -0300

    start hub

--- src/main.go
+++ src/main.go
@@ -4,6 +4,8 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"rogue-like/handlers"
+	"rogue-like/models"
 
 	"github.com/gorilla/mux"
 )
@@ -12,5 +14,13 @@ func main() {
 	log.Println("Hello")
 
 	r := mux.NewRouter()
+	hub := models.Hub{
+		Clients: make(map[*models.Client]bool),
+	}
+	go hub.Start()
+
+	r.HandleFunc("/ws/rogue-like/", func(w http.ResponseWriter, r *http.Request) {
+		handlers.RogueLikeHandler(&hub, w, r)
+	})
 	_ = http.ListenAndServe(":"+os.Getenv("PORT"), r)
 }
