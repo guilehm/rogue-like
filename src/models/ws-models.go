f8cd2467b020ca1900e31d039063bf6ceeff0074
guilherme <guile.hm@hotmail.com>
2022-01-07 21:26:47 -0300

    improve client and hub structs

--- src/models/ws-models.go
+++ src/models/ws-models.go
@@ -1,12 +1,21 @@
 package models
 
-import "github.com/gorilla/websocket"
+import (
+	"fmt"
+
+	"github.com/gorilla/websocket"
+)
 
 type Client struct {
-	Hub  *Hub
-	Conn *websocket.Conn
+	Hub    *Hub
+	Conn   *websocket.Conn
+	Player *Player
 }
 
 type Hub struct {
 	Clients map[*Client]bool
+}
+
+func (hub *Hub) Start() {
+	fmt.Println("starting")
 }
