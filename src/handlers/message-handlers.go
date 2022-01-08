ec48b176cc246813f21271f2324d2fa5badd649c
guilherme <guile.hm@hotmail.com>
2022-01-08 18:22:02 -0300

    refactor handle key down function

--- src/handlers/message-handlers.go
+++ src/handlers/message-handlers.go
@@ -5,11 +5,42 @@ import (
 	"log"
 	"rogue-like/models"
 	"rogue-like/services"
+	"rogue-like/settings"
 
 	"github.com/gorilla/websocket"
 )
 
-func handleUserJoins(s *services.GameService, conn *websocket.Conn, quit chan bool, message models.WSMessage) error {
+func handleKeyDown(client *models.Client, message models.WSMessage) error {
+	data := models.KeyDownMessage{}
+	err := json.Unmarshal(message.Data, &data)
+	if err != nil {
+		log.Println("error during unmarshall:", err)
+		return err
+	}
+
+	switch data.Key {
+	case models.ArrowLeft:
+		player := &client.Player
+		(*player).PositionX -= settings.MoveStep
+	case models.ArrowUp:
+		player := &client.Player
+		(*player).PositionY -= settings.MoveStep
+	case models.ArrowRight:
+		player := &client.Player
+		(*player).PositionX += settings.MoveStep
+	case models.ArrowDown:
+		player := &client.Player
+		(*player).PositionY += settings.MoveStep
+	}
+
+	client.Hub.Broadcast <- true
+
+	return nil
+}
+
+func handleUserJoins(
+	s *services.GameService, conn *websocket.Conn, client *models.Client, quit chan bool, message models.WSMessage,
+) error {
 
 	data := models.UserJoinsMessage{}
 	err := json.Unmarshal(message.Data, &data)
@@ -18,23 +49,20 @@ func handleUserJoins(s *services.GameService, conn *websocket.Conn, quit chan bo
 		return err
 	}
 	// TODO: sprite should not be hardcoded
-	sprite, err := s.GetSprite(models.Warrior)
+	sprite, err := s.GetSprite(models.Templar)
 	if err != nil {
 		log.Println(err.Error())
 		return err
 	}
 
-	client := &models.Client{
-		Hub:  s.Hub,
-		Conn: conn,
-		Player: &models.Player{
-			Sprite:    sprite,
-			Health:    sprite.HP,
-			PositionX: 0,
-			PositionY: 0,
-		},
+	client.Hub = s.Hub
+	client.Conn = conn
+	client.Player = &models.Player{
+		Sprite:    sprite,
+		Health:    sprite.HP,
+		PositionX: 0,
+		PositionY: 0,
 	}
-	s.Hub.Register <- client
 
 	go func() {
 		for {
@@ -45,5 +73,7 @@ func handleUserJoins(s *services.GameService, conn *websocket.Conn, quit chan bo
 			}
 		}
 	}()
+
+	s.Hub.Register <- client
 	return nil
 }
