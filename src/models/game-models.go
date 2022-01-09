38a5feaa42697416ba185d03527e6ef609427267
guilherme <guile.hm@hotmail.com>
2022-01-09 00:00:09 -0300

    simplify animation

--- src/models/game-models.go
+++ src/models/game-models.go
@@ -31,19 +31,19 @@ type Animation struct {
 }
 
 type Sprite struct {
-	Name            SpriteName  `json:"name"`
-	TileSet         TileSet     `json:"tileSet"`
-	SpriteX         int         `json:"spriteX"`
-	SpriteY         int         `json:"spriteY"`
-	SpriteWidth     int         `json:"spriteWidth"`
-	SpriteHeight    int         `json:"spriteHeight"`
-	HP              int         `json:"hp"`
-	MoveRange       int         `json:"moveRange"`
-	AttackRange     int         `json:"attackRange"`
-	XOffset         int         `json:"xOffset"`
-	YOffset         int         `json:"yOffset"`
-	AnimationPeriod int         `json:"animationPeriod"`
-	Animations      []Animation `json:"animations"`
+	Name            SpriteName `json:"name"`
+	TileSet         TileSet    `json:"tileSet"`
+	SpriteX         int        `json:"spriteX"`
+	SpriteY         int        `json:"spriteY"`
+	SpriteWidth     int        `json:"spriteWidth"`
+	SpriteHeight    int        `json:"spriteHeight"`
+	HP              int        `json:"hp"`
+	MoveRange       int        `json:"moveRange"`
+	AttackRange     int        `json:"attackRange"`
+	XOffset         int        `json:"xOffset"`
+	YOffset         int        `json:"yOffset"`
+	AnimationPeriod int        `json:"animationPeriod"`
+	Animation       Animation  `json:"animation"`
 }
 
 type Player struct {
