fa56023e5ab1dcc8cc3f3aaa023bf5452c8c1c18
guilherme <guile.hm@hotmail.com>
2022-01-17 19:27:57 -0300

    remove move range and set damage

--- models/game-models.go
+++ models/game-models.go
@@ -39,19 +39,20 @@ type Animation struct {
 }
 
 type Sprite struct {
-	Name            SpriteName `json:"name"`
-	TileSet         TileSet    `json:"tileSet"`
-	SpriteX         int        `json:"spriteX"`
-	SpriteY         int        `json:"spriteY"`
-	SpriteWidth     int        `json:"spriteWidth"`
-	SpriteHeight    int        `json:"spriteHeight"`
-	HP              int        `json:"hp"`
-	MoveRange       int        `json:"moveRange"`
-	AttackRange     int        `json:"attackRange"`
-	XOffset         int        `json:"xOffset"`
-	YOffset         int        `json:"yOffset"`
-	AnimationPeriod int        `json:"animationPeriod"`
-	Animation       Animation  `json:"animation"`
+	Name         SpriteName `json:"name"`
+	TileSet      TileSet    `json:"tileSet"`
+	SpriteX      int        `json:"spriteX"`
+	SpriteY      int        `json:"spriteY"`
+	SpriteWidth  int        `json:"spriteWidth"`
+	SpriteHeight int        `json:"spriteHeight"`
+	HP           int        `json:"hp"`
+	// MoveRange       int        `json:"moveRange"`
+	AttackRange     int       `json:"attackRange"`
+	Damage          int       `json:"damage"`
+	XOffset         int       `json:"xOffset"`
+	YOffset         int       `json:"yOffset"`
+	AnimationPeriod int       `json:"animationPeriod"`
+	Animation       Animation `json:"animation"`
 }
 
 type Coords struct {
