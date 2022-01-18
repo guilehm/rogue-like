4274e60407880ecd761baaee990503caffca53cc
guilherme <guile.hm@hotmail.com>
2022-01-17 21:12:55 -0300

    create drop and drop sprite structs

--- models/game-models.go
+++ models/game-models.go
@@ -47,6 +47,21 @@ type Animation struct {
 	YOffset      int `json:"yOffset"`
 }
 
+type DropSprite struct {
+	Name         DropName `json:"name"`
+	TileSet      TileSet  `json:"tileSet"`
+	SpriteX      int      `json:"spriteX"`
+	SpriteY      int      `json:"spriteY"`
+	SpriteWidth  int      `json:"spriteWidth"`
+	SpriteHeight int      `json:"spriteHeight"`
+}
+
+type Drop struct {
+	DropSprite DropSprite `json:"dropSprite"`
+	PositionX  int        `json:"positionX"`
+	PositionY  int        `json:"positionY"`
+}
+
 type Sprite struct {
 	Name         SpriteName `json:"name"`
 	TileSet      TileSet    `json:"tileSet"`
