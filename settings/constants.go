c8d2a743ab2465ac70305b8e7af26e6bd0566808
guilherme <guile.hm@hotmail.com>
2022-01-18 20:49:15 -0300

    create more constants

--- settings/constants.go
+++ settings/constants.go
@@ -1,5 +1,10 @@
 package settings
 
+import "time"
+
 const MoveStep = 1
 const MoveRange = 8
 const PercentageToAttackBack = 20
+const RespawnCheckTime = 1 * time.Second
+const IncreasePlayersHealthValue = 5
+const IncreasePlayersHealthCheckTime = 5 * time.Second
