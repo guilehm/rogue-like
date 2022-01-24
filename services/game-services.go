31513816367173ccd78b8a3c984f2c3f8fd78ffa
guilherme <guile.hm@hotmail.com>
2022-01-24 19:27:23 -0300

    use function to update hp

--- services/game-services.go
+++ services/game-services.go
@@ -71,10 +71,7 @@ func (s *GameService) CreateSprites() {
 			YOffset:      2,
 			Consume: func(drop *models.Drop, player *models.Player) {
 				drop.Consumed = true
-				player.Health += 20
-				if player.Health >= player.Sprite.HP {
-					player.Health = player.Sprite.HP
-				}
+				player.UpdateHP(20)
 			},
 		},
 	}
