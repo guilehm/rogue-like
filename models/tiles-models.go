f956af744cde50c11da2f83e1833b0b1072528a3
guilherme <guile.hm@hotmail.com>
2022-01-29 10:32:51 -0300

    create function to interruptr projectiles

--- models/tiles-models.go
+++ models/tiles-models.go
@@ -1,6 +1,8 @@
 package models
 
-import "rogue-like/helpers"
+import (
+	"rogue-like/helpers"
+)
 
 type TileSetData struct {
 	Width  int     `json:"width"`
@@ -22,23 +24,40 @@ func (l Layer) GetRowAndColumn(index int) (int, int) {
 	return row, column
 }
 
+func interruptProjectiles(v int) bool {
+	// water fields should not block projectiles
+	// but players cannot step into it
+
+	// water fields global ids from tiled:
+	allowedProjectilesValues := []int{170, 171, 186, 187}
+	for _, a := range allowedProjectilesValues {
+		if v == a {
+			return false
+		}
+	}
+	return true
+}
+
 func (l Layer) CreateTile(index, value int) Tile {
 	row, column := l.GetRowAndColumn(index)
+
 	tile := Tile{
-		Row:    row,
-		Column: column,
-		Value:  value,
+		Row:                  row,
+		Column:               column,
+		Value:                value,
+		InterruptProjectiles: interruptProjectiles(value),
 	}
 	tile.Area = tile.GetTileArea()
 	return tile
 }
 
 type Tile struct {
-	Index  int  `json:"index"`
-	Row    int  `json:"row"`
-	Column int  `json:"column"`
-	Area   Area `json:"area"`
-	Value  int  `json:"value"`
+	Index                int  `json:"index"`
+	Row                  int  `json:"row"`
+	Column               int  `json:"column"`
+	Area                 Area `json:"area"`
+	Value                int  `json:"value"`
+	InterruptProjectiles bool
 }
 
 func (t Tile) GetTileArea() Area {
